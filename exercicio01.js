be9f552c912d7d0b160d54c58f944771a1419fbe
BRAYAN JUNIOR ASTURIZAGA FLORES <24372090854@imaccommand017.local>
2025-11-18 10:53:35 -0300

    Exercicio 01

--- exercicio01.js
+++ exercicio01.js
@@ -27,7 +27,7 @@ let login = prompt("Digite o seu usuário:")
 let senha = prompt("Digite a sua senha:")
 
 if (login == "admin" && senha == "senha123") {
-    console.log("Login bem sucedido")
+    console.log("Login bem sucedido!")
 }
 else{
     console.log("Você não tem permissão de acesso!")
