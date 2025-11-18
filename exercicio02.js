b9cea272929c7cc76aea8df185674e7ccebcc56b
BRAYAN JUNIOR ASTURIZAGA FLORES <24372090854@imaccommand017.local>
2025-11-18 11:15:02 -0300

    Exercicio 02

---
+++ exercicio02.js
@@ -0,0 +1,35 @@
+
+let numero = Number.parseInt(prompt("Digite um número de 1 a 7"))
+switch (numero) {
+    case "1":
+        console.log("Segunda-Feira")
+        break;
+
+    case "2":
+        console.log("Terça-Feira")
+        break;
+
+    case "3":
+        console.log("Quarta-Feira")
+        break;
+
+    case "4":
+        console.log("Quinta-Feira")
+        break;
+
+    case "5":
+        console.log("Sexta-feira")
+        break;
+
+    case "6":
+        console.log("Sábado")
+        break;
+
+    case "7":
+        console.log("Domingo")
+        break;
+
+    default:
+        console.log("Não é um número de 1 a 7.")
+        break;
+}
---
+++ exercicio03.js
@@ -0,0 +1,12 @@
+// Faça um programa que receba um número do usuário e informe se este número é par ou
+// ímpar.
+
+let numero1 = window.prompt("Digite o número")
+if (numero1 % 2 ==)
+    {
+        console.log("Ele é par")
+    } 
+
+    else {
+        console.log("Ele é impar")
+    }
